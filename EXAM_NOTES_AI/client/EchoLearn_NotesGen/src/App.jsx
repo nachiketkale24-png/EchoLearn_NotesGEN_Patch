4cd56fe1eb547b6291ec961432d19aa987f51141
NachiketKale <nachiket.kale24@spit.ac.in>
2026-02-17 02:55:25 +0530

    Remove nested Router from App.jsx

--- EXAM_NOTES_AI/client/EchoLearn_NotesGen/src/App.jsx
+++ EXAM_NOTES_AI/client/EchoLearn_NotesGen/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Auth from './pages/auth';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 }
 

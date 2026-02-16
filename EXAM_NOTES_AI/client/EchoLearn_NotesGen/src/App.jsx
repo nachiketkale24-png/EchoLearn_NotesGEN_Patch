d124ee2b6137e44764663aeb5c524e67e8fc41c9
NachiketKale <nachiket.kale24@spit.ac.in>
2026-02-17 01:59:45 +0530

    Fix favicon and update index.html

--- EXAM_NOTES_AI/client/EchoLearn_NotesGen/src/App.jsx
+++ EXAM_NOTES_AI/client/EchoLearn_NotesGen/src/App.jsx
@@ -1,10 +1,14 @@
 import React from  'react';
+import { Routes } from 'react-router-dom';
 
 function App(){
   return(
-    <div>
-      <h1>Welcome to EchoLearn Notes Generator</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+      </Routes>
+    </>
   )
 }
 

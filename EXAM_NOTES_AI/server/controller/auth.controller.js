6bf1f28f4500bc151880654da1051be40c9239dc
NachiketKale <nachiket.kale24@spit.ac.in>
2026-02-20 12:18:54 +0530

    fix: auth.controller - remove stray brace, correct cookie option

---
+++ EXAM_NOTES_AI/server/controller/auth.controller.js
@@ -0,0 +1,23 @@
+import UserModel from "../models/user.model.js";
+import { getToken } from "../utils/token.js";
+
+export const googleAuth = async(req, res) => {
+    try{
+        const { email, name } = req.body;
+        let user = await UserModel.findOne({ email });
+        if(!user){
+            user = await UserModel.create({ name, email });
+        }
+        let token = await getToken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
--- EXAM_NOTES_AI/server/utils/token.js
+++ EXAM_NOTES_AI/server/utils/token.js
@@ -1,3 +1,12 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = async(userID) => {
+export const getToken = async(userID) => {
+    try{
+        const token = jwt.sign({userID}, process.env.JWT_SECRET_KEY, {expiresIn: "7d"});
+        console.log(token);
+        return token;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
